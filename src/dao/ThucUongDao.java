fb3b982e83139c817d8f8d405d269ed764ce2316
dat4567 <dathtcomputer@gmail.com>
2024-04-26 14:06:46 +0700

    xin chao

--- src/dao/ThucUongDao.java
+++ src/dao/ThucUongDao.java
@@ -50,7 +50,6 @@ public class ThucUongDao implements Dao<ThucUong>{
     
         int row = stmt.executeUpdate();
     }
-
     @Override
     public void update(ThucUong t) throws SQLException {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
