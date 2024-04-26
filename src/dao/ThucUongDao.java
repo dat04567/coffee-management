86b9e2aba96d780bf847d6470410f84b16ade5b2
dat4567 <dathtcomputer@gmail.com>
2024-04-26 14:13:12 +0700

    Revert "xin chao"
    This reverts commit fb3b982e83139c817d8f8d405d269ed764ce2316.

--- src/dao/ThucUongDao.java
+++ src/dao/ThucUongDao.java
@@ -50,6 +50,7 @@ public class ThucUongDao implements Dao<ThucUong>{
     
         int row = stmt.executeUpdate();
     }
+
     @Override
     public void update(ThucUong t) throws SQLException {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
