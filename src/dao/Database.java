b77c6ce416d709d7fb5f079175b9b8b82ed2d60e
dat4567 <dathtcomputer@gmail.com>
2024-05-06 12:57:52 +0700

    test aaaaaaa

--- src/dao/Database.java
+++ src/dao/Database.java
@@ -15,7 +15,7 @@ public class Database {
             Class.forName("com.mysql.cj.jdbc.Driver");
             String url = "jdbc:mysql://localhost:3306/coffee";
             String user = "root";
-            String password = "123";
+            String password = "123456";
             this.conn = DriverManager.getConnection(url, user, password);
             System.out.println("Kết nối cơ sở dữ liệu thành công!");
         } catch (SQLException e) {
